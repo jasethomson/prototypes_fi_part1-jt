b294cdc962f8e94ad6a4c456a18a7c8afa8b3609
Jase Thomson <jasethomson31@gmail.com>
2019-06-26 10:03:01 -0700

    JS | Functions - Jase Thomson

---
+++ js_functions/main.js
@@ -0,0 +1,12 @@
+
+function messageAlert(){
+  alert('Helloooooo');
+}
+function add(argument1, argument2){
+  var result = argument1 + argument2
+  console.log('result: ', result);
+}
+function addWithReturn(arg11, arg22){
+  return arg11 + arg22;
+}
+var addWithReturnResult = addWithReturn(11, 22);
