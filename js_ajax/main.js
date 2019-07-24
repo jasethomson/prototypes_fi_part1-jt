f43e1cee01fd0712e9b3bc95c8936fec1f445c16
Jase Thomson <jasethomson31@gmail.com>
2019-07-23 18:10:41 -0700

    JS | AJAX - Jase Thomson

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -4,6 +4,7 @@
 
 $(document).ready(initializeApp);
 
+var global_result = null;
 function initializeApp(){
     $('button').click(getData);
 }
@@ -14,9 +15,38 @@ function getData(){
         dataType: 'json',
         url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
         success: function(result) {
-            
             console.log('2) AJAX Success function called, with the following result:', result);
+            global_result = result;
+            var firstMovieInfo = global_result.feed.entry[0]["im:image"][2].label;
+            var accessTenMovies = global_result.feed.entry;
+            var lengthOfTopTenMovies = accessTenMovies.length;
 
+            var numOfTimesLoop = 0;
+            while(numOfTimesLoop< lengthOfTopTenMovies){
+                var thirdImage = accessTenMovies[numOfTimesLoop]["im:image"][2].label;
+                var divContainer = $("<div>", {
+                    class: "container",
+                    css: {
+                        'display': "inline-block",
+                        'width': "350px"
+                    }
+                });
+                var newImageTag = $("<img>", {
+                    src: thirdImage
+                });
+                var movieTitle = accessTenMovies[numOfTimesLoop]["im:name"].label;
+                var movieDirector = accessTenMovies[numOfTimesLoop]["im:artist"].label;
+                var movieTitleTag = $("<div>", {
+                    text: movieTitle
+                });
+                var movieDirectorTag = $("<div>", {
+                    text: movieDirector
+                });
+
+                $(divContainer).append(newImageTag, movieDirectorTag, "<br>", movieTitleTag, "<br>");
+                $("#main").append(divContainer);
+                numOfTimesLoop++;
+            }
         }
     }
 
